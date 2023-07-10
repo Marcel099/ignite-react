11269ff8dd322723c62b3c4267cbb3b54b818999
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-07-10 01:56:56 -0300

    P4 #14 - Paginação da lista de usuários

--- p04-dashgo/server/src/routes/usersRoutes.ts
+++ p04-dashgo/server/src/routes/usersRoutes.ts
@@ -5,8 +5,24 @@ import { prisma } from '../services/prisma'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/', async (req, res) => {
-    const users = await prisma.user.findMany()
-    return { users }
+    const paramsSchema = z.object({
+      page: z.string(),
+    })
+
+    const { page } = paramsSchema.parse(req.query)
+
+    const offset = page !== undefined
+      ? (Number(page) - 1) * 10
+      : 0
+
+    const users = await prisma.user.findMany({
+      skip: offset,
+      take: 10,
+    })
+
+    const total = await prisma.user.count()
+
+    return { users, total }
   })
   
   app.post('/', async (req, res) => {
